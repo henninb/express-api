253073213f4246e717d2b00101411d56ca29ab5f
Brian Henning <henninb@msn.com>
2023-05-23 05:47:49 -0500

    wip

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,3 @@
-module.exports = (req, res) => {
-  res.status(200).json({ message: 'Hello vercel!' });
+module.exports = (_request, response) => {
+  response.status(200).json({ message: 'Hello vercel!' });
 };
