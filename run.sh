fb4e9a1c5d60c70e6e72685cdd66c3dd1a7daf53
Brian Henning <henninb@msn.com>
2023-05-31 17:39:32 -0500

    wip

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ echo 'http://localhost:3000'
 
 npm install
 echo npm start
+docker stop express-api
+docker rm -f express-api
+docker rmi express-api
+docker compose -f docker-compose.yml up -d
 echo node server.js
 
 exit 0
