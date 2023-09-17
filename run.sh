d06f5620ebd8744c36596d4e2d4ac110003d84de
Brian Henning <henninb@msn.com>
2023-09-17 16:22:36 -0500

    wip

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ docker rm -f express-api
 docker rmi express-api
 docker compose -f docker-compose.yml up -d
 echo npx npm-check-updates -u
+echo https://localhost:8080
 echo node server.js
 
 exit 0
